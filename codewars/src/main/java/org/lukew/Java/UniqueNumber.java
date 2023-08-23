c91c3626ceac9342b0f1e7f26d6b2051c5993279
Luke Williams <lukewilliams10101@gmail.com>
2023-08-23 14:27:11 +0200

    refactor: UniqueNumber refactored to handle large arrays

--- codewars/src/main/java/org/lukew/Java/UniqueNumber.java
+++ codewars/src/main/java/org/lukew/Java/UniqueNumber.java
@@ -1,18 +1,13 @@
 package org.lukew.Java;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 public class UniqueNumber {
+//    There is an array with some numbers. All numbers are equal except for one. Try to find it
     public static double findUniq(double arr[]) {
-        for (int i = 0; i < arr.length; i++) {
-            int count = 0;
-            for (int j = 0; j < arr.length; j++) {
-                if (arr[i] == arr[j]) {
-                    count++;
-                }
-            }
-            if (count == 1) {
-                return arr[i];
-            }
-        }
-        return 0.0;
+        Arrays.sort(arr);
+        return arr[0] == arr[1] ? arr[arr.length - 1] : arr[0];
     }
 }
