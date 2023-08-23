a97df35474f3cafae6da2e02370412f4222016a2
Luke Williams <lukewilliams10101@gmail.com>
2023-08-23 15:40:53 +0200

    feat: factorial solved 2.0

--- codewars/src/main/java/org/lukew/Java/Factorial.java
+++ codewars/src/main/java/org/lukew/Java/Factorial.java
@@ -1,2 +1,17 @@
-package org.lukew.Java;public class Factorial {
+package org.lukew.Java;
+
+public class Factorial {
+
+//  Write a function to calculate factorial for a given input. If input is below 0 or above
+//  12 throw an IllegalArgumentException (Java).
+    public int factorial(int n) {
+        if (n < 0 || n > 12) throw new IllegalArgumentException();
+
+        int result = 1;
+
+        for (int i = n; i > 0; i--) {
+            result *= i;
+        }
+        return result;
+    }
 }
--- codewars/src/test/java/org/lukew/Java/FactorialTest.java
+++ codewars/src/test/java/org/lukew/Java/FactorialTest.java
@@ -1,4 +1,28 @@
+package org.lukew.Java;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class FactorialTest {
-  
+
+    Factorial fact = new Factorial();
+
+    @Test
+    public void test_factorial0() {
+        assertEquals(1, fact.factorial(0));
+    }
+
+    @Test
+    public void test_factorial3() {
+        assertEquals(6, fact.factorial(3));
+    }
+
+    @Test
+    public void test_factorial5() {
+        assertEquals(120, fact.factorial(5));
+    }
+
 }
