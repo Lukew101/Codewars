411e09abf4a722635eeb7c5152fb06b67778cb9a
Luke Williams <lukewilliams10101@gmail.com>
2023-08-28 11:56:00 +0200

    feat: smallEnough solved

--- codewars/src/main/java/org/lukew/Java/YourOrderPlease.java
+++ codewars/src/main/java/org/lukew/Java/YourOrderPlease.java
@@ -1,7 +1,6 @@
 package org.lukew.Java;
 
-import java.util.Map;
-import java.util.TreeMap;
+import java.util.Arrays;
 
 public class YourOrderPlease {
 //    Your task is to sort a given string. Each word in the string will contain a single number.
@@ -19,5 +18,11 @@ public class YourOrderPlease {
             }
         }
         return result.toString().trim();
+    }
+
+    // You will be given an array and a limit value. You must check that all values in the array
+    // are below or equal to the limit value. If they are, return true. Else, return false.
+    public static boolean smallEnough(int[] a, int limit) {
+        return Arrays.stream(a).allMatch(num -> num <= limit);
     }
 }
--- codewars/src/test/java/org/lukew/Java/YourOrderPleaseTest.java
+++ codewars/src/test/java/org/lukew/Java/YourOrderPleaseTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.assertEquals;
 
 class YourOrderPleaseTest {
     @Test
@@ -19,5 +20,13 @@ class YourOrderPleaseTest {
     @Test
     public void test3() {
         assertThat("Empty input should return empty string", YourOrderPlease.order(""), equalTo(""));
+    }
+
+    @Test
+    public void basicTests() {
+        assertEquals(true, YourOrderPlease.smallEnough(new int[] { 66, 101 }, 200));
+        assertEquals(false, YourOrderPlease.smallEnough(new int[] { 78, 117, 110, 99, 104, 117, 107, 115 }, 100));
+        assertEquals(true, YourOrderPlease.smallEnough(new int[] { 101, 45, 75, 105, 99, 107 }, 107));
+        assertEquals(true, YourOrderPlease.smallEnough(new int[] { 80, 117, 115, 104, 45, 85, 112, 115 }, 120));
     }
 }
